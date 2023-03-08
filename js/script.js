2479901779f60f69f2bc4f233d2b3794b244aec3
silvanoguel <silvanoguel@msn.com>
2023-03-08 16:04:51 +0100

    add:project completed

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const UserAge = parseInt(prompt("Quanti anni hai ?"));
 console.log(UserKm, UserAge);
 
 
-// ELABORAZIONE DATI
+// ELABORAZIONE DATI con OUTPUT
 
 // calcolo prezzo intero del biglietto
 const ticketPrice = (0.21 * UserKm)
@@ -15,8 +15,26 @@ console.log(ticketPrice);
 
 // calcolo prezzo biglietto con i 2 sconti
 // per minorenni
-const specialPrice1 = parseInt (ticketPrice - (ticketPrice * 0.2));
+const specialPrice1 = parseInt(ticketPrice - (ticketPrice * 0.2));
 // per over 65
-const specialPrice2 = parseInt (ticketPrice - (ticketPrice * 0.4));
+const specialPrice2 = parseInt(ticketPrice - (ticketPrice * 0.4));
 
+// calcolo
 
+if (UserAge < 18) {
+    specialPrice1.toFixed(2);
+    console.log (specialPrice1);
+    alert ('Hai diritto ad uno sconto del 20%');
+    document.getElementById("price").innerHTML =
+    `Il costo del tuo biglietto è: ${specialPrice1.toFixed(2)} €`;
+} else if (UserAge >= 65) {
+    specialPrice2;
+    console.log(specialPrice2)
+    alert ('Hai diritto ad uno sconto del 40%');
+    document.getElementById("price").innerHTML =
+    `Il costo del tuo biglietto è: ${specialPrice2.toFixed(2)} €`;
+} else {
+    ticketPrice;
+    document.getElementById("price").innerHTML =
+    `Il costo del tuo biglietto è: ${ticketPrice.toFixed(2)} €`;
+}
